48b9d6d157168ff862e5e67f7ba6166106d5cffc
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-01-16 09:04:50 +0530

    Add index file

---
+++ src/Components/index.js
@@ -0,0 +1,11 @@
+import Container from "./Container/Container";
+import Header from './Header/Header'
+import LogoutBtn from "./Header/LogoutBtn";
+import Footer from './Footer/Footer'
+import Button from "./Button";
+import Input from "./Input";
+import Logo from './Logo'
+
+export {
+    Container, Header, LogoutBtn, Footer, Button, Input, Logo
+}
