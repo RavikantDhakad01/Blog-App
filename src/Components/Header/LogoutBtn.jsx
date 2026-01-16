bb0fccc3afbb4f1a94d9472d5e53504c39dd93a4
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-01-16 08:51:34 +0530

    Add Logout button

---
+++ src/Components/Header/LogoutBtn.jsx
@@ -0,0 +1,21 @@
+import authService from '../../appwrite/auth'
+import { logout } from '../../store/authslice'
+import { useDispatch } from 'react-redux'
+
+function LogoutBtn() {
+
+    const dispatch = useDispatch()
+
+    function logoutHandler() {
+        authService.logout().then(() => {
+            dispatch(logout())
+        })
+    }
+
+    return (
+        <button onClick={logoutHandler} className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">
+            Logout
+        </button>
+    )
+}
+export default LogoutBtn
