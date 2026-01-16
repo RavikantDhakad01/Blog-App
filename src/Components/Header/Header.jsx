275d54787daa57e97969ae93dc6bfbdf64fe566c
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-01-16 08:50:21 +0530

    Add Header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,70 @@
+import { Logo, LogoutBtn, Container } from '../index'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+function Header() {
+   const navigate = useNavigate()
+   const authStatus = useSelector((state) =>state.auth.status)
+   const navItems = [
+      {
+         name: "Home",
+         path: '/',
+         active: true
+      },
+      {
+         name: "Login",
+         path: '/login',
+         active: !authStatus
+      },
+      {
+         name: "Signup",
+         path: '/signup',
+         active: !authStatus
+      },
+      {
+         name: "All posts",
+         path: '/all-posts',
+         active: authStatus
+      },
+      {
+         name: "Create post",
+         path: '/create-post',
+         active: authStatus
+      }
+   ]
+   return (
+      <>
+         <header className='py-3 shadow bg-gray-500'>
+            <Container>
+               <nav className='flex'>
+                  <div className='mr-4'>
+                     <Link to='/'>
+                        <Logo width='70px' />
+                     </Link>
+
+                  </div>
+
+                  <ul className='flex ml-auto'>
+                     {
+                        navItems.map((item) => {
+                           return item.active ? (
+                              <li key={item.name}>
+                                 <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                                    onClick={() => { navigate(item.path) }}
+                                 >{item.name}</button>
+                              </li>
+                           ) : null
+                        })
+                     }
+                     {
+                        authStatus && <li>
+                           <LogoutBtn />
+                        </li>
+                     }
+                  </ul>
+               </nav>
+            </Container>
+         </header>
+      </>
+   )
+}
+export default Header 
