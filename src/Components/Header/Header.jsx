260dcfbaed6fbcf5974a87c12126b6692a0ae20e
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-01-20 21:42:05 +0530

    Add cursor pointer

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -48,7 +48,7 @@ function Header() {
                         navItems.map((item) => {
                            return item.active ? (
                               <li key={item.name}>
-                                 <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                                 <button className='cursor-pointer inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
                                     onClick={() => { navigate(item.path) }}
                                  >{item.name}</button>
                               </li>
