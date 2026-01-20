78de48ed7e3286fac729438efe0e7af0c8fe6f00
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-01-20 21:41:43 +0530

    Add cursor pointer

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,7 +1,7 @@
 function Button({ children,type, bgColor = "bg-blue-600", textColor = "text-white", className = "", ...props }) {
     return (
 
-        <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props} type={type}>
+        <button className={`cursor-pointer px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props} type={type}>
             {children}
         </button>
     )
