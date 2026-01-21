3f848afed55d8cb7b9e783a1f545bdcf08902355
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-01-21 17:00:59 +0530

    fix path issues

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -2,7 +2,7 @@
 function Logo({ size = "w-24" }) {
     return (
 
-        <img src="../../public/favicon.png" alt="Logo" className={`${size} h-auto object-contain`} />
+        <img src="/favicon.png" alt="Logo" className={`${size} h-auto object-contain`} />
     )
 }
 export default Logo
