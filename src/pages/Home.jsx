98c7a88939a013434e621890c4dd2872951f915d
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-01-21 20:25:05 +0530

    fix posts alignment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,10 +36,10 @@ const authStatus=useSelector((state)=>state.auth.status)
 
        <div className='w-full py-8'>
             <Container>
-                <div className='flex flex-wrap'>
+                <div className='flex flex-wrap justify-center'>
                     {
                         posts.map((post) => (
-                            <div key={post.$id} className='p-2 w-1/4'>
+                            <div key={post.$id} className='p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4'>
                                 <PostCard {...post} />
                             </div>
                         ))
