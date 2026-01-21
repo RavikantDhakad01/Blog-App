03939b1696dcd65a18663a5b9fee856f90d0823d
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-01-21 08:35:48 +0530

    Show post to all

--- src/Components/pages/AllPosts.jsx
+++ src/Components/pages/AllPosts.jsx
@@ -1,24 +1,22 @@
 import { useEffect, useState } from "react";
 import { Container, PostCard } from '../index'
 import service from "../../appwrite/config";
-import { useSelector } from "react-redux";
+
 
 function AllPosts() {
-    const userData =useSelector((state)=>state.auth.user)
+
     const [Posts, setPosts] = useState([])
 
     useEffect(() => {
-        if(!userData){
-            return
-        }
-        service.getPosts(userData.$id)
+        
+        service.getPosts()
             .then((posts) => {
                 if (posts) {
                     setPosts(posts.documents)
                 }
 
             })
-    }, [userData])
+    }, [])
     return (
         <div className='w-full py-8'>
             <Container>
